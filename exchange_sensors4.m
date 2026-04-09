eec69cef0d92026a3053ff39b803335edbe4b83f
mcheng05 <98044039+mcheng05@users.noreply.github.com>
2026-04-09 18:58:13 -0400

    exchanges with stochastic greedy

---
+++ exchange_sensors4.m
@@ -0,0 +1,104 @@
+function [p,ld_exchange, swap_count, ld_orig] = exchange_sensors4(p,x,sig_n,sig_f,ls,f, eps)
+% Function that takes initial placements and improves selection by improving
+% the determinant by a factor f and via stochastic greedy algorithm
+arguments (Input)
+    p
+    x
+    sig_n
+    sig_f (1,1) double = 1.0 % Default value if not provided
+    ls (1,1) double = 1.0 % Default value if not provided
+    f (1,1) double = 1.0 % Default value if not provided
+    eps (1,1) double = 0.5 % Default value if not provided
+end
+
+arguments (Output)
+    p
+    ld_exchange
+    swap_count
+    ld_orig
+end
+swap_count = 0;
+
+k = length(p);
+n = length(x);
+
+% functions for getting elements of K
+K_fun = @(x) gaussKern(x,sig_f,ls);
+K_fun_offdiag = @(x,x2) gaussKern(x,sig_f,ls,x2);
+K_diag = @(x) gaussKern(x,sig_f,ls,'1');
+
+alpha = @(i) 1/(sig_n^2)*K_diag(x(i,:))+1; % "on" diagonal elements
+A = @(i,j) 1/(sig_n^2)*K_fun_offdiag(x(i,:),x(j,:));  % off diagonal elements
+
+A_p = (1/(sig_n^2))*K_fun(x(p,:))+eye(k);
+L = chol(A_p, 'lower'); % Compute the Cholesky factor of the covariance matrix
+
+ld_orig = slogdet(K_fun(x(p,:)), sig_n); % orig ld
+
+for l = 1:k
+    %% --- FAST DOWNDATE (O(k^2) Method) ---
+    % gets the cholesky factor of downdated matrix, L_fast ready to work sift through
+    % n-k unselected sensors
+
+    idx = 1; % since swaps are added to end of selection index list, just take first element each time
+        
+    col_to_add = L(2:end,1);
+    L_fast = L(2:end,2:end);
+    L_fast = (cholupdate(L_fast', col_to_add, '+'))';
+
+    %% --- 2. VECTORIZED EXCHANGE SEEKING STEP ---
+    % note: adds new sensor to "end" of block
+    p_temp = p;
+    p_temp(1) = []; 
+
+    %test_ind = setdiff(1:n, p_temp);
+    
+    % sample s = ceil{n/k ln(1/eps)} points
+    s = ceil(n/k*log(1/eps));
+    test_ind = randsample(1:n, s, false);
+
+    % compute b vectors for randomly sampled points
+    test_ind = setdiff(test_ind, p_temp); % filter out already-selected sensors
+    
+    
+    % compute all b vectors for this sweep to make more efficient
+    rhs_all = A(p_temp,test_ind);
+    b_all = L_fast \ rhs_all;
+
+    % A_diag = diag(A);
+    A_test_diag = alpha(test_ind); %  A_diag(test_ind); %  % Only get diagonals for candidates
+
+    % compute all betas for this sweep
+    beta_all = sqrt(A_test_diag' - sum(b_all.^2, 1));
+
+    % compute beta of unswapped selection
+    rhs_orig = A(p_temp, p(idx));
+    if size(rhs_orig, 2) > 1; rhs_orig = rhs_orig'; end
+    
+    b_orig = L_fast \ A(p_temp, p(1));
+    beta_orig = sqrt(alpha(p(idx)) - b_orig'*b_orig);
+
+    % swap decision
+    [max_beta, i] = max(beta_all);
+    best_sensor_ind = test_ind(i); % Map back to absolute sensor ID
+
+    % Add a tiny tolerance (1e-9) to prevent swapping identical values
+    if max_beta > beta_orig*sqrt(f) + 1e-9
+        % Grab the correct b vector using the relative index
+        b_best = b_all(:, i); 
+        
+        p = [p_temp best_sensor_ind];
+        L = [L_fast zeros(k-1,1); b_best' max_beta];
+        disp("swap made in round "+l)
+        swap_count = swap_count +1;
+    else 
+        % Push original sensor to the back of the queue
+        % disp("no swap made in round "+l)
+        p = circshift(p, -1);
+        L = [L_fast zeros(k-1,1); b_orig' beta_orig(1)];
+    end
+
+end
+disp("total swaps: "+swap_count)
+ld_exchange = slogdet(K_fun(x(p,:)), sig_n)
+end
